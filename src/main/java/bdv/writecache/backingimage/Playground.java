f91077b6342a7ec903688f3aa64c52c437526a4d
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-08 18:42:04 -0500

    Use generic RandomAccessibleCacheLoader from imglib2-cache

--- src/main/java/bdv/writecache/backingimage/Playground.java
+++ src/main/java/bdv/writecache/backingimage/Playground.java
@@ -3,8 +3,6 @@ package bdv.writecache.backingimage;
 import static net.imglib2.cache.img.DiskCachedCellImgOptions.options;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Iterator;
 
 import org.scijava.ui.behaviour.DragBehaviour;
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
@@ -21,126 +19,45 @@ import net.imglib2.RandomAccessible;
 import net.imglib2.algorithm.neighborhood.HyperSphereShape;
 import net.imglib2.algorithm.neighborhood.Neighborhood;
 import net.imglib2.cache.CacheLoader;
+import net.imglib2.cache.img.AccessFlags;
 import net.imglib2.cache.img.DiskCachedCellImgFactory;
 import net.imglib2.cache.img.DiskCachedCellImgOptions;
 import net.imglib2.cache.img.DiskCachedCellImgOptions.CacheType;
+import net.imglib2.cache.img.RandomAccessibleCacheLoader;
 import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.img.basictypeaccess.array.DirtyLongArray;
-import net.imglib2.img.basictypeaccess.array.LongArray;
 import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
-import net.imglib2.img.cell.LazyCellImg;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.realtransform.RealViews;
-import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.LongType;
-import net.imglib2.util.Fraction;
-import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 
 public class Playground
 {
-	public static class RandomAccessibleLoader<
-				T extends NativeType< T >,
-				A extends ArrayDataAccess< A > >
-			implements CacheLoader< Long, Cell< A > >
-	{
-		private final CellGrid grid;
-
-		private final RandomAccessible< T > source;
-
-		private final T type;
-
-		private final A creator;
-
-		private final Fraction entitiesPerPixel;
-
-		public RandomAccessibleLoader(
-				final CellGrid grid,
-				final RandomAccessible< T > source,
-				final T type,
-				final A creator )
-		{
-			this.grid = grid;
-			this.source = source;
-			this.type = type;
-			this.creator = creator;
-			entitiesPerPixel = type.getEntitiesPerPixel();
-		}
-
-		@Override
-		public Cell< A > get( final Long key ) throws Exception
-		{
-			final long index = key;
-
-			final int n = grid.numDimensions();
-			final long[] cellMin = new long[ n ];
-			final int[] cellDims = new int[ n ];
-			final long[] imgDims = new long[ n ];
-			final long[] cellMax = new long[ n ];
-
-			grid.getCellDimensions( index, cellMin, cellDims );
-			final long numEntities = entitiesPerPixel.mulCeil( Intervals.numElements( cellDims ) );
-			final A data = creator.createArray( ( int ) numEntities );
-
-			for ( int d = 0; d < n; ++d )
-			{
-				cellMax[ d ] = cellMin[ d ] + cellDims[ d ] - 1;
-				imgDims[ d ] = cellDims[ d ];
-			}
-
-			final ArrayImg< T, A > img = new ArrayImg<>(
-					( A ) new LongArray( ( long[] ) data.getCurrentStorageArray() ),
-					imgDims, entitiesPerPixel );
-			try
-			{
-				LazyCellImg.linkType( type, img );
-			}
-			catch ( NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e )
-			{
-				throw new RuntimeException( e );
-			}
-			final Iterator< T > it = Views.interval( source, cellMin, cellMax ).iterator();
-			for ( final T t : img )
-				t.set( it.next() );
-
-			return new Cell<>( cellDims, cellMin, data );
-		}
-	}
-
 	public static void main( final String[] args ) throws IOException
 	{
 		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
 
-		final long scale = 1000;
-
 		final MandelbrotRealRandomAccessible mb = new MandelbrotRealRandomAccessible();
+
+		final long scale = 1000;
 		final AffineTransform2D transform = new AffineTransform2D();
 		transform.set(
 				scale, 0, 2 * scale,
 				0, scale, 2 * scale );
 		final RandomAccessible< LongType > source = RealViews.affine( mb, transform );
 
-
 		final int[] cellDimensions = new int[] { 512, 512 };
 		final long[] dimensions = new long[] { 4 * scale, 4 * scale };
 
-		final CacheLoader< Long, Cell< DirtyLongArray > > loader = new RandomAccessibleLoader<>(
-				new CellGrid( dimensions, cellDimensions ),
-				source,
-				new LongType(),
-				new DirtyLongArray( 0 ) );
+		final CacheLoader< Long, Cell< DirtyLongArray > > loader = RandomAccessibleCacheLoader.get( new CellGrid( dimensions, cellDimensions ), source, AccessFlags.DIRTY );
 		final DiskCachedCellImgOptions options = options()
 				.cellDimensions( cellDimensions )
 				.cacheType( CacheType.BOUNDED );
-		final Img< LongType > img = new DiskCachedCellImgFactory< LongType >( options ).create(
-				dimensions,
-				new LongType(),
-				loader );
+		final Img< LongType > img = new DiskCachedCellImgFactory< LongType >( options ).create( dimensions, new LongType(), loader );
 
 		// Hack: add and remove dummy source to avoid that the initial transform shows a full slice.
 		final BdvStackSource< ARGBType > dummy = BdvFunctions.show( ArrayImgs.argbs( 100, 100 ), "Dummy", BdvOptions.options().is2D() );
